b36688a53d9399cf9479c7f493d9734c5326a193
MGYOSBEL <mgyosbel@gmail.com>
2020-12-17 12:54:44 +0100

    menu toggle OK'

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./OrderSummary.module.css";
 import Auxiliary from "../../../hoc/Auxiliary";
-import Button from '../../UI/Button/Button';
+import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((ingrKey) => {
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map((ingrKey) => {
+      return (
+        <li key={ingrKey}>
+          <span style={{ textTransform: "capitalize" }}>{ingrKey}</span>:{" "}
+          {this.props.ingredients[ingrKey]}
+        </li>
+      );
+    });
     return (
-      <li key={ingrKey}>
-        <span style={{textTransform: 'capitalize'}}>{ingrKey}</span>: {props.ingredients[ingrKey]}
-      </li>
+      <Auxiliary className={classes.OrderSummary}>
+        <h3>Your Order</h3>
+        <p>A delicious burguer with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <hr />
+        <p>
+          <strong>Price: {this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button buttonType="Danger" clicked={this.props.canceled}>
+          CANCEL
+        </Button>
+        <Button buttonType="Success" clicked={this.props.accepted}>
+          CONTINUE
+        </Button>
+      </Auxiliary>
     );
-  });
-  return (
-    <Auxiliary className={classes.OrderSummary}>
-      <h3>Your Order</h3>
-      <p>A delicious burguer with the following ingredients:</p>
-      <ul>
-          {ingredientSummary}
-      </ul>
-      <hr />
-      <p><strong>Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button buttonType='Danger' clicked={props.canceled}>CANCEL</Button>
-      <Button buttonType='Success' clicked={props.accepted}>CONTINUE</Button>
-    </Auxiliary>
-  );
-};
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,11 +2,13 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle toggle={props.menuToggle}/>
         <Logo />
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
     </header>
